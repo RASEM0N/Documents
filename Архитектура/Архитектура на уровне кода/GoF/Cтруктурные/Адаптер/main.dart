443dfb5c4eb91308eca361c88020937b5fa642b1
Роман <negativ12fly@gmail.com>
2024-01-07 03:17:43 +0500

    feat: паттерн адаптер

---
+++ Архитектура/Архитектура на уровне кода/GoF/Cтруктурные/Адаптер/main.dart
@@ -0,0 +1,34 @@
+void main() {
+  var animalTransport = new AnimalToTransportAdapter();
+  var driver = new Driver(animalTransport);
+  driver.travel()
+}
+
+class Driver {
+  late final Transport _transport;
+
+  Driver(this._transport);
+
+  void travel() {
+    this._transport.drive();
+  }
+}
+
+abstract class Transport {
+  void drive();
+}
+
+class AnimalToTransportAdapter implements Transport {
+  final Animal _animal = new Animal();
+
+  @override
+  void drive() {
+    _animal.move();
+  }
+}
+
+class Animal {
+  void move() {
+    // ...
+  }
+}
